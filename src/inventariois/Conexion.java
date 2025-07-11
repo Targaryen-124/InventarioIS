00a6519bc27df5f61b8df28a021217943b2c4b95
Targaryen-124 <ypadilla1988@gmail.com>
2025-07-11 12:30:56 -0600

    Actualizacion
    Actualizacion

--- src/inventariois/Conexion.java
+++ src/inventariois/Conexion.java
@@ -8,34 +8,32 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import javax.swing.JOptionPane;
 
-/**
- *
- * @author Yohana Padilla
- */
 public class Conexion {
     Connection conectar = null;
-    
+
+    // Datos de conexión
     String usuario = "root";
-    String contra ="";
+    String contra = ""; 
     String bd = "inventarios";
     String ip = "localhost";
     String puerto = "3306";
-    
-    String cadena = "jdbc:mysql://"+ip+":"+puerto+"/"+bd;
-    
-    public Connection estableceConexion(){
-        
-        try{
-            
+
+    // Cadena de conexión con zona horaria
+    String cadena = "jdbc:mysql://" + ip + ":" + puerto + "/" + bd;
+
+    public Connection estableceConexion() {
+        try {
+            // Cargar el driver
             Class.forName("com.mysql.cj.jdbc.Driver");
-            conectar = DriverManager.getConnection(cadena,usuario,contra);
-            JOptionPane.showMessageDialog(null,"La conexion se a realizado con exito");
             
-        } catch (Exception e){
+            // Establecer la conexión
+            conectar = DriverManager.getConnection(cadena, usuario, contra);
             
-            JOptionPane.showMessageDialog(null, "Error a conectar a la base de datos: "+ e.toString());   
+            
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "❌ Error al conectar: " + e.toString());
         }
-        
+
         return conectar;
     }
 }
